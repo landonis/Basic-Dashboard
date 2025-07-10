c44bcbba60bee44868c6a9273af6f42736aa652b
Landon Diedrich <43480252+landonis@users.noreply.github.com>
2025-07-09 22:13:12 -0400

    Update index.ts

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -22,6 +22,10 @@ app.use('/api', authRoutes);
 
 app.use(express.static(path.join(__dirname, '../../frontend/dist')));
 
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../../frontend/dist/index.html'));
+});
+
 app.use(errorHandler);
 
 app.listen(PORT, () => {
